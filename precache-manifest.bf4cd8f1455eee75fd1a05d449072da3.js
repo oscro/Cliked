8ca64edbf4448830fd4ecdf573d34f79f4c62497
Oscro <oscar.orchilles@gmail.com>
2019-04-08 10:34:33 -0400

    Updates

---
+++ precache-manifest.bf4cd8f1455eee75fd1a05d449072da3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Cliked/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9416a1b66fff8ef956ac",
+    "url": "/Cliked/static/js/runtime~main.81eb8927.js"
+  },
+  {
+    "revision": "b1022b9abc84a80550a9",
+    "url": "/Cliked/static/js/main.18d719a9.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Cliked/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "b1022b9abc84a80550a9",
+    "url": "/Cliked/static/css/main.44d14de5.chunk.css"
+  },
+  {
+    "revision": "b4a1ff395ed43b8c6612090f9f1e20c8",
+    "url": "/Cliked/index.html"
+  }
+];
