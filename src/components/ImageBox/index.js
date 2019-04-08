99395af99e53962c278ad03a50392f3b3711882c
Oscro <oscar.orchilles@gmail.com>
2019-04-08 17:39:37 -0400

    React

--- src/components/ImageBox/index.js
+++ src/components/ImageBox/index.js
@@ -5,22 +5,68 @@ import "./style.css"
 class ImageBox extends Component {
 
     state = {
-        MemoryImages
+        MemoryImages,
+        clicked: 0 
     };
 
-    render() {
-        return (
-            <div className="row">
-            {this.state.MemoryImages.map(image => (
-                <img 
-                className="img-thumbnail rounded float-left col-6" 
-                src={image.image} 
-                alt=""
-                />
-            ))}
-            </div>
-        );
+    handleClick = event => {
+        let click = this.state.clicked + 1;
+        
+        // event.target.data-clicked + 1;
+
+        this.setState({
+            clicked: click
+        });
+
+        console.log(this.state.clicked);
+
+    }
+
+    randomize = function (array) {
+        
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+        
+        console.log(array);
+        this.setState({ MemoryImages: array });
     };
+    // {this.randomize(this.state.MemoryImages)}
+    
+
+render() {
+    
+    return (
+        <div>
+            {this.state.MemoryImages.map(image => (
+                <div 
+                onClick={() => this.handleClick()}
+                >
+                <img
+                    className="img-thumbnail rounded float-left col-6"
+                    src={image.image}
+                    alt=""
+                    data-clicked={this.props.clicked}
+                    onClick={()=>this.randomize(this.state.MemoryImages)}
+                />
+                </div>
+            ))}
+        </div>
+    );
+};
 }
+
+
 
 export default ImageBox;
--- src/components/MemPick/index.js
+++ src/components/MemPick/index.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import ImageBox from "../ImageBox";
+import MemoryImages from "../../gameImages.json";
 import "./style.css"
 
-function MemPick() {
+class MemPick extends Component {
 
-  return (
-    <div>
-      <div className="container">
-      <ImageBox />
-      </div>
-    </div>
-  );
+    state = {
+        MemoryImages,
+        clicked: 0
+    }
+
+    // handleClick = event => {
+    //     let click = this.state.clicked + 1;
+        
+    //     // event.target.data-clicked + 1;
+
+    //     this.setState({
+    //         clicked: click
+    //     });
+
+    //     console.log(this.state.clicked);
+
+    // }
+
+    render() {
+        return (
+            <div>
+                <div className="container">
+                <div className="row">
+                    <ImageBox 
+                    // clicked={this.state.clicked}
+                    // onClick={() => this.handleClick()}
+                    />
+                </div>
+                </div>
+            </div>
+        );
+
+    }
 }
 
 export default MemPick;
