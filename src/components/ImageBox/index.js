438963ce3eaac3bfc7a0c166cefee67ebb53701e
Oscro <oscar.orchilles@gmail.com>
2019-04-09 15:18:56 -0400

    React Changes

--- src/components/ImageBox/index.js
+++ src/components/ImageBox/index.js
@@ -6,68 +6,77 @@ class ImageBox extends Component {
 
     state = {
         MemoryImages,
-        clicked: 0 
+        clicked: 0
     };
 
-    handleClick = (event, array) => {
+    handleClick = () => {
         let click = this.state.clicked + 1;
-        
         // event.target.data-clicked + 1;
-
         this.setState({
             clicked: click
         });
-
         // ()=>image.image.clicked = image.image.clicked + 1)
-
         console.log(this.state.clicked);
-
     }
 
-    randomize = function (array) {
-        
-        var currentIndex = array.length, temporaryValue, randomIndex;
+    // src={this.state.MemoryImages[this.props.key].image}
 
-        // While there remain elements to shuffle...
-        while (0 !== currentIndex) {
-
-            // Pick a remaining element...
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-
-            // And swap it with the current element.
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
-        
-        console.log(array);
-        this.setState({ MemoryImages: array });
-    };
-    // {this.randomize(this.state.MemoryImages)}
-    
-
-render() {
-    
-    return (
-        <div>
-            {this.state.MemoryImages.map(image => (
-                <div 
-                onClick={() => this.handleClick()}
-                >
+    render() {
+        return (
+            <div className="col-6">
                 <img
-                    className="img-thumbnail rounded float-left col-6"
-                    src={image.image}
+                    className="img-thumbnail rounded float-left"
+                    src={this.props.src}
                     alt=""
-                    data-clicked={this.props.clicked}
-                    onClick={()=>this.randomize(this.state.MemoryImages)}
+                    onClick={this.handleClick}
                 />
-                </div>
-            ))}
-        </div>
-    );
-};
+            </div>
+        );
+    };
 }
+
+// render() {
+
+//     return (
+//         <div>
+//             {this.state.MemoryImages.map(image => (
+//                 <div 
+//                 onClick={() => this.handleClick()}
+//                 >
+//                 <img
+//                     className="img-thumbnail rounded float-left col-6"
+//                     src={image.image}
+//                     alt=""
+//                     data-clicked={this.props.clicked}
+//                     onClick={()=>this.randomize(this.state.MemoryImages)}
+//                 />
+//                 </div>
+//             ))}
+//         </div>
+//     );
+// };
+
+// randomize = function (array) {
+
+//     var currentIndex = array.length, temporaryValue, randomIndex;
+
+//     // While there remain elements to shuffle...
+//     while (0 !== currentIndex) {
+
+//         // Pick a remaining element...
+//         randomIndex = Math.floor(Math.random() * currentIndex);
+//         currentIndex -= 1;
+
+//         // And swap it with the current element.
+//         temporaryValue = array[currentIndex];
+//         array[currentIndex] = array[randomIndex];
+//         array[randomIndex] = temporaryValue;
+//     }
+
+//     console.log(array);
+//     this.setState({ MemoryImages: array });
+// };
+// {this.randomize(this.state.MemoryImages)}
 
 
 
--- src/components/MemPick/index.js
+++ src/components/MemPick/index.js
@@ -3,36 +3,56 @@ import ImageBox from "../ImageBox";
 import MemoryImages from "../../gameImages.json";
 import "./style.css"
 
+
 class MemPick extends Component {
 
     state = {
-        MemoryImages,
-        clicked: 0
+        // An array of keys
+        keys: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     }
 
-    // handleClick = event => {
-    //     let click = this.state.clicked + 1;
-        
-    //     // event.target.data-clicked + 1;
+    // createIds = (MemoryImages) => {
+    //     let arry = [];
+    //     for (let i = 0; i < MemoryImages.length; i++) {
+    //         arry.push(MemoryImages[i].id);
+    //     }
+    //     return arry;
+    // }
 
-    //     this.setState({
-    //         clicked: click
-    //     });
+    randomize = function (items) {
+        // this.state.keys.sort(function (a, b) {
+        //     return (0.5 - Math.random());
+        // });
+        return items[Math.floor(Math.random() * items.length)];
+    }
 
-    //     console.log(this.state.clicked);
+    // iterator = function (arry) {
+
+    //     for(let i = 0; i < arry.length; i ++) {
+    //         {this.displayImage(MemoryImages, this.randomize(this.state.keys))}
+    //     }
 
     // }
+
+    displayImage = function (arry, key) { 
+        return (
+            <ImageBox
+                src={arry[key].image}
+            // clicked={this.state.clicked}
+            // onClick={() => this.handleClick()}
+            />
+        );
+    }
 
     render() {
         return (
             <div>
                 <div className="container">
-                <div className="row">
-                    <ImageBox 
-                    // clicked={this.state.clicked}
-                    // onClick={() => this.handleClick()}
-                    />
-                </div>
+                    <div className="row">
+                    {this.state.keys.map(keys => (
+                    this.displayImage(MemoryImages, this.randomize(this.state.keys))
+                    ))}
+                    </div>
                 </div>
             </div>
         );
